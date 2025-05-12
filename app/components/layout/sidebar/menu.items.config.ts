838d9c1555225662f9a7c5216fd72ad329c5f018
HDUY102 <leduy020702@gmail.com>
2025-05-12 22:40:46 +0700

    Update layout

--- app/components/layout/sidebar/menu.items.config.ts
+++ app/components/layout/sidebar/menu.items.config.ts
@@ -1,4 +1,4 @@
-import {Archive,ArchiveX,LogOut,LayoutDashboard,LayoutList,ChartNoAxesCombined,Flower,Bell,Settings,HelpCircle,CircleUser} from "lucide-react";
+import {Contact,Archive,ArchiveX,LayoutDashboard,LayoutList,ChartNoAxesCombined,Flower} from "lucide-react";
 
 type SidebarMenuItem = {
   title: string;
@@ -21,13 +21,18 @@ export const menuItems: SidebarMenuItem[] = [
     href: "/dashboard",
   },
   {
-    title: "hehe",
+    title: "Sản phẩm",
     icon: LayoutList ,
     children: [
       { title: "Hoa tươi", icon: Flower, href: "/dashboard/freshflowers" },
       { title: "Hoa giả", icon: Flower, href: "/dashboard/artificialflowers" },
       { title: "Phụ kiện", icon: Flower, href: "/dashboard/accessory" },
     ],
+  },
+  {
+    title: "Khách hàng",
+    icon: Contact,
+    href: "/dashboard/customers",
   },
   {
     title: "Thống kê",
@@ -47,21 +52,3 @@ export const menuItems: SidebarMenuItem[] = [
     ],
   }
 ]
-
-export const headerItems: HeaderMenuItem[] = [
-  {
-    title: "Thông báo",
-    icon: Bell,
-    href: "/notifications",
-  },
-  {
-    title: "Tài khoản",
-    icon: CircleUser,
-    children: [
-      { title: "Thông tin cá nhân", icon: CircleUser, href: "/account/profile" },
-      { title: "Cài đặt", icon: LogOut, href: "/settings" },
-      { title: "Trợ giúp", icon: LogOut, href: "/help" },
-      { title: "Đăng xuất", icon: LogOut, href: "/logout" },
-    ],
-  }
-];
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ import RightLine from '../components/RightSide'
 import Footer from '../components/Footer'
 const DashboardChildrent = ({children}:{children: React.ReactNode}) => {
   return (
-    <div className='grid grid-cols-1 grid-rows-5 md:grid-cols-[auto_1fr_100px]
+    <div className='grid grid-cols-1 grid-rows-5 md:h-screen md:grid-cols-[auto_1fr_100px]
      md:grid-rows-[100px_1fr_150px] text-black bg-white'>
         <div className='row-start-1 bg-red-500 md:row-start-1 md:col-start-2 md:col-span-2'>
             <TopHeader/>
@@ -13,7 +13,7 @@ const DashboardChildrent = ({children}:{children: React.ReactNode}) => {
         <div className='row-start-2 bg-amber-300 md:row-start-1 md:col-start-1 md:row-span-3'>
             <Sidebar/>
         </div>
-        <div className='row-start-3 bg-blue-600 md:row-start-2 md:col-start-2'>{children}</div>
+        <div className='row-start-3 md:row-start-2 md:col-start-2'>{children}</div>
         <div className='row-start-4 bg-emerald-400 md:row-start-2 md:col-start-3 md:row-span-2'>
             <RightLine/>
         </div>
