67f3a8f2d21e8b228557eba4a2ebeb3d72944a5b
HDUY102 <leduy020702@gmail.com>
2025-06-03 23:09:10 +0700

    Chagne style tailwindcss

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,10 +12,10 @@ const DashboardChildrent = ({children}:{children: React.ReactNode}) => {
     return (
     <div className={`grid grid-cols-[100px_1fr] grid-rows-[70px_1fr_100px] h-screen md:grid-cols-[100px_1fr_100px]
         md:grid-rows-[80px_1fr_150px] text-white ${isOpenSidebar ? 'md:grid-cols-[auto_1fr_100px]' : 'md:grid-cols-[100px_1fr_100px]'}`}>
-        <div className='row-start-1 col-start-2 bg-red-500 md:row-start-1 md:col-start-2 md:col-span-2'>
+        <div className='row-start-1 col-start-2 bg-gray-800 md:row-start-1 md:col-start-2 md:col-span-2'>
             <TopHeader/>
         </div>
-        <div className={`row-span-3 md:row-start-1 md:col-start-1 md:row-span-3 bg-slate-800 text-white ${isOpenSidebar? 'absolute md:static h-full' : 'md:block'}`}>
+        <div className={`row-span-3 md:row-start-1 md:col-start-1 md:row-span-3 bg-gray-900 text-white ${isOpenSidebar? 'absolute h-full md:static z-50' : 'md:block'}`}>
             <Sidebar isOpenSidebar={isOpenSidebar} toggleSidebar={toggleSidebar}/>
         </div>
         <div className='row-start-2 col-start-2 md:row-start-2 md:col-start-2 text-black'>{children}</div>
