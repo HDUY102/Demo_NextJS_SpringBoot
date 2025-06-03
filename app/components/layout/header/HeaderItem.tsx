cc0522129db6e3e90ea22ed3fcbd86608222849e
HDUY102 <leduy020702@gmail.com>
2025-06-03 23:00:58 +0700

    Design Header

--- app/components/layout/header/HeaderItem.tsx
+++ app/components/layout/header/HeaderItem.tsx
@@ -7,7 +7,7 @@ interface HeaderItemProps {
 }
 export default function HeaderItems({icon:Icon, href}:HeaderItemProps){
     return(
-        <Link href={href} className="pr-4 hover:text-black">
+        <Link href={href} className="pr-4 hover:text-blue-400">
             <Icon size={20}/>
         </Link>
     )
--- app/components/layout/header/header.items.config.ts
+++ app/components/layout/header/header.items.config.ts
@@ -1,8 +1,7 @@
-import { BellIcon, PanelRightClose, Search, UserCircle } from "lucide-react";
+import { BellIcon, PanelRightClose, UserCircle } from "lucide-react";
 
 export const headerItems = [
     {title: 'sidebar', icon: PanelRightClose, href:''},
-    {title: 'search', icon: Search, href:'/search'},
-    {title: 'bell', icon: BellIcon, href:'/bell'},
+    {title: 'bell', icon: BellIcon, href:''},
     {title: 'user', icon: UserCircle, href:'/user'},
 ]
