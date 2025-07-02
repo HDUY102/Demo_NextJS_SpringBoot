a1b911160f7d35b448fe85223a36eddf2894054f
HDUY102 <leduy020702@gmail.com>
2025-07-02 21:42:17 +0700

    Fix order page in dashboard

--- app/dashboard/order/page.tsx
+++ app/dashboard/order/page.tsx
@@ -4,17 +4,55 @@ import TableHeader from '../../components/table/TableHeader'
 import { Orders, useOrders } from '@/hooks/useOrders';
 import { Column } from '@/app/components/table/table.item';
 import TableBody from '@/app/components/table/TableBody';
+import axiosInstance from '@/lib/axios';
+import TableCategory from '@/app/components/table/TableCategory';
 
 export default function OrderPage () {
-  const { orders, isLoading, isError } = useOrders();
-  const [expandedRows, setExpandedRows] = useState<Record<string, boolean>>({});
+  const { orders, isLoading, isError,mutate } = useOrders();
 
+  const [expandedRows, setExpandedRows] = useState<Record<string, boolean>>({});
   const toggleRow = (id: string) => {
     setExpandedRows((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
-  if (isLoading) return <div className="p-4">Loading...</div>
-  if (isError) return <div className="p-4 text-red-500">Failed to load orders</div>
+  const handleConfirm = async (orderId: string | number) => {
+    try {
+      const newStatusId = 2; // New -> Processing
+      await axiosInstance.put( `/orders/${orderId}/status`,
+        {
+            currentStatusId: newStatusId,
+            note: 'Xác nhận đơn hàng',
+          // Use Params URL
+          // params: {
+          //   orderId,
+          //   newStatusId,
+          //   note: 'Xác nhận đơn hàng',
+          // },
+        },
+      );
+
+      console.log("✅ Đơn đã được xác nhận:");
+
+      mutate();
+  }catch (error: any) {
+    console.error("❌ Lỗi khi xác nhận đơn:", error);
+    alert("Cập nhật trạng thái thất bại!");
+  }};
+
+  const handleCancel = async (orderId: string | number) => {
+    try{
+      const newStatusId = 6; // New -> Canceled
+      await axiosInstance.put(`orders/${orderId}/status`,{
+          currentStatusId: newStatusId,
+          note: 'Đã hủy đơn hàng'
+        })
+      console.log("❌ Đơn đã hủy:");
+
+      mutate();
+    }catch (error: any){
+      console.error("❌ Lỗi khi xác nhận đơn:", error);
+      alert("Cập nhật trạng thái thất bại!");
+  }};
 
   const orderColumns: Column<Orders>[] = [
     {
@@ -36,11 +74,9 @@ export default function OrderPage () {
       key: "details",
       label: "Details",
       render: (order) => (
-        <button
-          className="text-blue-600 underline"
-          onClick={() => toggleRow(order.id)}
-        >
-          {expandedRows[order.id] ? "Ẩn" : "Xem"}
+        <button className="text-blue-700 underline hover:cursor-pointer" 
+          onClick={() => toggleRow(order.id)}>
+            {expandedRows[order.id] ? "Ẩn" : "Xem"}
         </button>
       ),
     },
@@ -51,12 +87,38 @@ export default function OrderPage () {
     {
       key: "isPaid",
       label: "Paid?",
-      render: (order) => (order.isPaid ? "Paid" : "Unpaid"),
+      render: (order) => (order.isPaid ? 
+        <div className='rounded-2xl bg-green-800 p-0.5 text-center'>
+          Đã thanh toán
+        </div> : <div className='rounded-2xl bg-amber-500 p-0.5 text-center'>
+          Chưa thanh toán
+        </div>),
+    },
+    {
+      key: "actions",
+      label: "Actions",
+      render: (order) => (
+        <div >
+          {<div className="flex gap-2">
+              <button className="px-2 py-1 bg-green-700 text-white text-xs rounded hover:bg-green-600 hover:cursor-pointer"
+                onClick={() => handleConfirm(order.id)}>
+                Xác nhận
+              </button>
+              <button className="px-2 py-1 bg-red-600 text-white text-xs rounded hover:bg-red-500 hover:cursor-pointer"
+                onClick={() => handleCancel(order.id)} > Hủy
+              </button>
+            </div>}
+      </div>
+      ),
     },
   ];
 
+  if (isLoading) return <div className="p-4">Loading...</div>
+  if (isError) return <div className="p-4 text-red-500">Failed to load orders</div>
+  console.log(orders)
   return (
     <div className="p-4">
+      <TableCategory />
       <div className="shadow-2xl border rounded">
         <table className="min-w-full divide-y divide-gray-200 text-sm">
           <TableHeader columns={orderColumns} />
