ac38552deac1e8d88cae493b6358560d5d1103e0
HDUY102 <leduy020702@gmail.com>
2025-08-31 14:17:51 +0700

    Set up  HoChiMinh time zone to display consistently with database

--- backend/Demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/Demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import java.util.TimeZone;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example") 	
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh")); // Set time zone at HoChiMinh (UTC +7)
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
