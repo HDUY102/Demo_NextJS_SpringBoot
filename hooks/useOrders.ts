90a28f59dffc17573df238cf4cb5b21a58bfe39d
HDUY102 <leduy020702@gmail.com>
2025-09-13 18:05:03 +0700

    Change await axiosInstance to pagination

--- hooks/useOrders.ts
+++ hooks/useOrders.ts
@@ -29,15 +29,59 @@ export interface OrderStatusHistory {
   updatedAt?: string;
 }
 
-export const useOrders = (keyword: string) =>{
-    const {data, error, isLoading, mutate} = useSWR<Orders[]>(
-        ['/orders', keyword],
-        ([url, keyword]) => postFetcher(url, { keyword }))
-    // const {data, error, isLoading, mutate} = useSWR<Orders[]>('/orders',fetcher)
-    return{
-        orders: data,
+export interface Page<T> {
+    content: T[];
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+}
+
+// export const useOrders = (keyword: string) =>{
+//     const {data, error, isLoading, mutate} = useSWR<Orders[]>(
+//         ['/orders', keyword],
+//         ([url, keyword]) => postFetcher(url, { keyword }))
+//     // const {data, error, isLoading, mutate} = useSWR<Orders[]>('/orders',fetcher)
+//     return{
+//         orders: data,
+//         isLoading,
+//         isError: error,
+//         mutate
+//     }
+// }
+
+const getFetcher = async (url: string) => {
+    const response = await axiosInstance.get(url);
+    return response.data;
+};
+export const useOrders = (
+    keyword: string,
+    page: number,
+    size: number,
+    sortBy: string,
+    sortDir: 'ASC' | 'DESC'
+) => {
+    const params = new URLSearchParams();
+    params.append('page', page.toString());
+    params.append('size', size.toString());
+    params.append('sortByDateOrder', sortBy);
+    params.append('sortDir', sortDir);
+    if (keyword) {
+        params.append('keyword', keyword);
+    }
+    
+    const url = `/orders/pagination?${params.toString()}`;
+    
+    const { data, error, isLoading, mutate } = useSWR<Page<Orders>>(
+        url,
+        getFetcher
+    );
+    
+    return {
+        orders: data?.content,
+        totalPages: data?.totalPages,
         isLoading,
         isError: error,
-        mutate
-    }
-}
+        mutate,
+    };
+};
