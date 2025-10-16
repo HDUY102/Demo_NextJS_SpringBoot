77ef5637f5b2e7b6b467edb56c0ec1a434f57d38
HDUY102 <leduy020702@gmail.com>
2025-10-16 22:16:04 +0700

    Change define Table Column, Table Record in Generic

--- app/components/table/table.item.ts
+++ app/components/table/table.item.ts
@@ -1,14 +1,29 @@
+// Define Type
+type ViewFieldType = "view" //custom view on table
+type ButtonFieldType = "button" //custom button on table
+type TextFieldType = "text" //Fill data in table
+type ExpandFieldType = 'expand' //Expand row hide on table
+type BooleanFieldType = 'bool' //Set true/false field on table
+type DateFieldType = 'date' //Set data has type Date
+
 // Table Column
-export interface Column<T> {
-  key: keyof T | string;
-  label: string;
-  render?: (item: T, index?: number) => React.ReactNode;
-  isSortable?: boolean;
+export interface TableColumn<M, K extends keyof M>{
+  key: K
+  label: string
+  isSortable?: boolean
+  type: ViewFieldType | ButtonFieldType | TextFieldType | ExpandFieldType | BooleanFieldType | DateFieldType
+}
+
+// Table Record
+export interface TableRecord<T, K extends keyof T>{
+  key: K
+  type: ViewFieldType | ButtonFieldType | TextFieldType | ExpandFieldType | BooleanFieldType | DateFieldType
+  renderCell?: (key: K, value: T[K], row: T) => React.ReactNode
 }
 
 // Table Header
 export interface TableHeaderProps<T> {
-  columns: Column<T>[];
+  columns: TableColumn<T, keyof T>[];
   onSort: (columnKey: string) => void;
   filterValues: Record<string, FilterOption[]>;
   onFilter: (key: string, value: string | null) => void;
@@ -17,18 +32,21 @@ export interface TableHeaderProps<T> {
   onKeyDown?: (key: string, event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
+// Table Body
+export interface TableBodyProps<T> {
+  records: Array<TableRecord<T,keyof T>>
+  data: T[];
+  expandedRows?: Record<string, boolean>;
+  onToggleRow: (id: string) => void;
+  onConfirm: (orderId: string | number) => void;
+  onCancel: (orderId: string | number) => void;
+}
+
 // Sort Data
 export type SortDirection = 'asc' | 'desc'
 export interface SortState<T>{
   key: keyof T | string | null;
   direction: null | SortDirection;
-}
-
-// Table Body
-export interface TableBodyProps<T> {
-  data: T[];
-  columns: Column<T>[];
-  expandedRows?: Record<string, boolean>;
 }
 
 // Table Category
@@ -58,4 +76,23 @@ export interface TableFilterProps<T> {
 export interface FilterOption {
   value: string; // yyyy-mm-dd
   label: string; // dd/mm/yyyy
+}
+
+// Payment Order
+export interface PaymentStatusBadgeProps {
+  isPaid: boolean;
+}
+
+// Show Detail Order
+export interface ShowDetailProps{
+    orderId: string
+    isExpanded: boolean
+    onClick: (id: string) => void
+}
+
+// Table Action
+export interface TableActionProps {
+    orderId: string
+    onConfirm: (id: string) => void
+    onCancel: (id: string) => void
 }
