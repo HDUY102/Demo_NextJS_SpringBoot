8ea8812784b58ce4a79430a4a4665b651ce06b47
HDUY102 <leduy020702@gmail.com>
2025-10-16 22:16:29 +0700

    Change tailwind css

--- app/components/table/TableAction.tsx
+++ app/components/table/TableAction.tsx
@@ -4,12 +4,12 @@ import { TableActionProps } from './table.item'
 export const TableAction: React.FC<TableActionProps> = ({orderId, onConfirm, onCancel}) => {
   return (
     <div className="flex gap-2">
-        <button className='px-2 py-1 bg-green-700 text-white text-xs rounded hover:bg-green-600 hover:cursor-pointer'
+        <button className='px-2 py-1 text-blue-600 text-xs rounded hover:text-blue-400 hover:cursor-pointer'
             onClick={() => onConfirm(orderId)}
         >
             Xác nhận
         </button>
-        <button className='px-2 py-1 bg-red-600 text-white text-xs rounded hover:bg-red-500 hover:cursor-pointer'
+        <button className='px-2 py-1 text-red-600 text-xs rounded hover:text-red-400 hover:cursor-pointer'
             onClick={() => onCancel(orderId)}
         >
             Hủy
