221b7bd693d708ef85433f14355dc7eff07a0960
HDUY102 <leduy020702@gmail.com>
2025-10-16 22:17:46 +0700

    Define logic render in component for generic

--- app/components/table/TableBody.tsx
+++ app/components/table/TableBody.tsx
@@ -1,30 +1,70 @@
 import React from 'react';
-import {TableBodyProps } from './table.item';
+import {TableBodyProps, TableRecord } from './table.item';
 import TableDetail from '@/app/components/table/TableDetail';
+import { PaymentStatus } from './PaymentStatus';
+import { TableAction } from './TableAction';
+import { ShowDetail } from './ShowDetail';
+import { Orders } from '@/hooks/useOrders';
 
-const TableBody = <T extends { id?: string | number }>({ data, columns,expandedRows = {}}: TableBodyProps<T>) => {
-  return (
-    <tbody className='bg-gray-900 text-white'>
-      {data.map((row, index) => (
-        <React.Fragment key={(row.id || index).toString()}>
-          <tr className="hover:bg-gray-800 border-b">
-            {columns.map((col, i) => (
-              <td key={i} className="px-4 py-2 text-sm">
-                {col.render ? col.render(row,index) : String((row as any)[col.key])}
-              </td>
-            ))}
+const TableBody = <T,>({records, data, expandedRows, onToggleRow, onConfirm, onCancel} : TableBodyProps<T>) =>{
+  return(
+    <tbody className="bg-gray-900 text-white">
+      {data.map((row, rowIndex)=>{
+        const rowId = (row as any).id as string
+        const isCurrentRowExpanded = expandedRows?.[rowId] || false;
+        return(
+          <React.Fragment key={(row as any).id}>
+          <tr key={rowIndex} className='hover:bg-gray-800 transition duration-150'>
+            {records.map((col: TableRecord<T, keyof T>)=>{
+              const value = row[col.key]
+              let cellContent: React.ReactNode
+              
+              if(col.renderCell){
+                cellContent = col.renderCell(col.key, value, row)
+              }else{
+                switch(col.type){
+                  case 'bool':
+                    cellContent = <PaymentStatus isPaid={Boolean(value)} />
+                    break;
+                  case 'button':
+                    cellContent = <TableAction orderId={String(col.key)} onConfirm={onConfirm} onCancel={onCancel}/>
+                    break;
+                  case 'expand':
+                    cellContent = <ShowDetail orderId={rowId} isExpanded={isCurrentRowExpanded} onClick={onToggleRow} />
+                    break;
+                  case 'text':
+                    cellContent = <span>{String(value ?? "")}</span>;
+                    break;
+                  case 'view':
+                    cellContent = <span className="text-center">{rowIndex + 1}</span>;
+                    break;
+                  case 'date':
+                    cellContent = <span> {new Date((row as any).dateOrder).toLocaleDateString("vi-VN")} </span>
+                    break;
+                  default: 
+                    cellContent = <span> - </span>
+                }
+              }
+              return (
+                <td key={String(col.key)} className={`px-2 py-4 text-sm ${
+                      col.key === "isPaid" ? "text-center" : "text-left" }`}
+                >
+                  {cellContent}
+                </td>
+            )})}
           </tr>
-          {"details" in row && expandedRows[row.id as string] && (
-            <tr className="bg-gray-50">
-              <td colSpan={columns.length} className="px-4 py-2">
-                <TableDetail details={(row as any).details} />
+          {isCurrentRowExpanded && (
+            <tr>
+              <td colSpan={records.length} className="bg-gray-700 p-0">
+                <TableDetail details={(row as Orders).details || []}/>
               </td>
             </tr>
           )}
         </React.Fragment>
-      ))}
+        )
+      })}
     </tbody>
-  );
-};
+  )
+}
 
 export default TableBody;
